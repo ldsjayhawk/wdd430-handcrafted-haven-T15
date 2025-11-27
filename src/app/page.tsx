3b8a35e1b4e1f787e338a2d160dec848d977047a
ldsjayhawk <crisbecca@gmail.com>
2025-11-27 11:20:17 -0600

    updated css; mobile version of home page to buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import styles from "./page.module.css";
+import Link from 'next/link';
+import font from 'next/font/google'
 
 export default function Page() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.intro}>
-          <h1>Home Page</h1>
+          <h1>Discover Unique
+          Handcrafted Treasures</h1>
           <p>
-            Webpage under construction....{" "}
+            Support local artisans and bring home one-of-a-kind pieces crafted with passion and skill.
           </p>
+            <Link href="/shop" className={styles.primary}>
+              Shop Now
+            </Link>
+            <Link href="/artisans" className={styles.secondary}>
+              Meet Our Artisans
+            </Link>
         </div>
       </main>
     </div>
